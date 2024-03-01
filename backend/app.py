4f48c4f47568a55709326665b4eaed97dd4b6b92
Armaan Vakharia <armaanvakharia@gmail.com>
2024-02-29 18:23:26 -0600

    Resolved TLS errors on Windows

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import certifi
 from flask import Flask
 from flask_cors import CORS
 from pymongo import MongoClient
@@ -7,10 +8,14 @@ app = Flask(__name__)
 CORS(app, supports_credentials=True) 
 app.config.from_object(config['development'])
 
+import certifi
+ca = certifi.where()
+
 # Connect to MongoDB
-client = MongoClient(app.config['MONGO_URI'])
+print(app.config['MONGO_URI'])
+client = MongoClient(app.config['MONGO_URI'], tlsCAFile=ca)
 mongo = client.get_database("461l")
- 
+
 from routes import * 
 # from project_routes import *
 
