fa078929aae5c8e9dcc1f511468cd0d37203b44b
Armaan Vakharia <43391096+armaan-v924@users.noreply.github.com>
2024-03-21 13:44:58 -0500

    Add .env file to .gitignore and update config.py and requirements.txt

--- backend/config.py
+++ backend/config.py
@@ -1,10 +1,13 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
     DEBUG = True
     MONGO_URI = os.environ.get('MONGO_URI')
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    PORT = 5001
+    PORT = os.getenv('PORT', 5001)
 
 config = {
     'development': Config
